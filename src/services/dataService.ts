3311eae0048bb0cfe2e883cd370bf5c73951b7bb
erayaras <erayaras@msn.com>
2023-10-21 00:23:52 +0300

    Add service

---
+++ src/services/dataService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://nesine-case-study.onrender.com';
+
+export const fetchData = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}/bets`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
+};
