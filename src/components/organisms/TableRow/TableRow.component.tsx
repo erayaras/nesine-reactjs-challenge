47ef701e5ebd817d190b43c1bd34c1d7fae06d41
erayaras <erayaras@msn.com>
2023-10-22 00:23:53 +0300

    Fix Displayed value for the row content

--- src/components/organisms/TableRow/TableRow.component.tsx
+++ src/components/organisms/TableRow/TableRow.component.tsx
@@ -6,7 +6,7 @@ import styles from './TableRow.module.scss';
 
 const TableRow: React.FC<TableRowProps> = ({ bet }) => {
     const headers = [`${bet.D} ${bet.DAY} ${bet.LN}`, "Yorumlar", "", "1", "x", "2", "Alt", "Üst", "H1", "1", "x", "2", "H2", "1-X", "1-2", "X-2", "Var", "Yok", "+99"]
-    const contents = [`${bet.C} ${bet.T} ${bet.N}`, "Yorumlar", bet.OCG[1].MBS, bet.OCG[1].OC[0].O, bet.OCG[1].OC[1].O, "", bet.OCG[5].OC[25].O, bet.OCG[5].OC[26].N, "", "", "", "", "", bet.OCG[2].OC[3].O, bet.OCG[2].OC[4].O, bet.OCG[2].OC[5].O, "", "", "3"]
+    const contents = [`${bet.C} ${bet.T} ${bet.N}`, "Yorumlar", bet.OCG[1].MBS, bet.OCG[1].OC[0].O, bet.OCG[1].OC[1].O, "", bet.OCG[5].OC[25].O, bet.OCG[5].OC[26].O, "", "", "", "", "", bet.OCG[2].OC[3].O, bet.OCG[2].OC[4].O, bet.OCG[2].OC[5].O, "", "", "3"]
 
     return (
         <table className={styles.rowContainer}>
