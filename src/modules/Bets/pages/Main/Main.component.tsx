9db569d0ea1820157aece3e037dfd2f2bc74ffc7
erayaras <erayaras@msn.com>
2023-10-23 02:00:56 +0300

    Add react-window in main screen

--- src/modules/Bets/pages/Main/Main.component.tsx
+++ src/modules/Bets/pages/Main/Main.component.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { FixedSizeList as List } from 'react-window';
 import BetSummary from '../../../../components/atoms/BetSummary/BetSummary.component';
 import StickyHeader from '../../../../components/molecules/StickyHeader/StickyHeader.component';
 import TableRow from '../../../../components/organisms/TableRow/TableRow.component';
@@ -10,6 +11,8 @@ const Main: React.FC = () => {
     const [data, setData] = useState<Bet[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<Error | null>(null);
+    const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+
 
     useEffect(() => {
         const loadData = async () => {
@@ -24,17 +27,36 @@ const Main: React.FC = () => {
         };
 
         loadData();
+
+        // Update the height when the window is resized
+        const handleResize = () => {
+            setViewportHeight(window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup the event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
     }, []);
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error.message}</div>;
 
+    const Row = ({ index, style }: { index: number; style: React.CSSProperties }) => {
+        return <div style={style}><TableRow bet={data[index]} /></div>;
+    };
+
     return (
         <div>
             <StickyHeader totalEventCount={data.length} />
-            {data.map((bet, index) => (
-                <TableRow key={index} bet={bet} />
-            ))}
+            <List
+                height={viewportHeight}  // Set to the viewport height
+                itemCount={data.length}
+                itemSize={53} // Average height of a row
+                width="100%"
+            >
+                {Row}
+            </List>
             <BetSummary />
         </div>
     );
